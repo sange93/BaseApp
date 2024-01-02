cc45cdd3e65f36215b22cb8f866c43c11f864d0a
sangshengqi <sangshengqi@qltyx.com>
2024-01-02 11:47:40 +0800

    升级 AGP 8.2.0;

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -57,9 +57,9 @@ dependencies {
 //    implementation "org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version"
 //    implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version"
 //    implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version"
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -83,8 +83,8 @@ dependencies {
     // 约束布局
     api("androidx.constraintlayout:constraintlayout:2.1.4")
     // 添加Jetpack中架构组件的依赖，注意viewmodel要添加viewmodel-ktx的依赖
-    api("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    api("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    api("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    api("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     // Kotlin Coroutines 协程
     api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     // ViewPager2
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.1")
+        classpath("com.android.tools.build:gradle:8.2.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     }
 }
 
 plugins {
     // Android Gradle plugin and Android Studio 对应关系: https://developer.android.google.cn/studio/releases/
-    id("com.android.application") version "8.1.1" apply false
-    id("com.android.library") version "8.1.1" apply false
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
     // kotlin编译器与kotlin版本对应关系：https://developer.android.google.cn/jetpack/androidx/releases/compose-kotlin#kts
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false// kotlin插件版本
     // 声明 KSP 插件。请务必选择与项目的 Kotlin 版本一致的 KSP 版本。https://developer.android.google.cn/studio/build/migrate-to-ksp?hl=zh-cn
