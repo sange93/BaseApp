b017a97f4b4ea2b51a0750d6cc97a3e4fad235ba
ssq <sangsq@foxmail.com>
2024-05-17 17:54:39 +0800

    版本"1.1.4";

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.github.sange93"
-version = "1.1.3"
+version = "1.1.4"
 
 android {
     namespace = "com.sange.base"
@@ -126,7 +126,7 @@ afterEvaluate {
             create<MavenPublication>("release"){
                 groupId = "com.github.sange93"
                 artifactId = "BaseApp"
-                version = "1.1.3"
+                version = "1.1.4"
                 from(components["release"])
             }
         }
