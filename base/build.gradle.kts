1b1afd49887a9fdf772ae55684e8d7bb62869ada
ssq <sangsq@foxmail.com>
2024-05-17 17:53:20 +0800

    1、迁移版本管理至toml文件; 2、升级lib版本；

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -42,7 +42,7 @@ android {
     }
     composeOptions {
         // kotlin编译器与kotlin版本对应关系：https://developer.android.google.cn/jetpack/androidx/releases/compose-kotlin#kts
-        kotlinCompilerExtensionVersion = "1.5.8"
+        kotlinCompilerExtensionVersion = "1.5.13"
     }
     packaging {
         resources {
@@ -52,73 +52,71 @@ android {
 }
 
 dependencies {
-    val lifecycleVersion = "2.6.2"
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 
     //-----------以下为定制内容------------
-    api("androidx.activity:activity-compose:1.8.2")
+    api(libs.androidx.activity.compose)
     // compose最新Bom版本：https://developer.android.google.cn/jetpack/compose/bom?hl=en
     // Bom内Lib详细版本：https://developer.android.google.cn/jetpack/compose/bom/bom-mapping?hl=en
-    val composeBom = platform("androidx.compose:compose-bom:2024.01.00")
+    val composeBom = platform("androidx.compose:compose-bom:2024.05.00")
     api(composeBom)
-    api("androidx.compose.ui:ui")
-    api("androidx.compose.ui:ui-graphics")
-    api("androidx.compose.ui:ui-tooling-preview")
-    api("androidx.compose.material3:material3")
-    api("androidx.compose.material:material")
+    api(libs.androidx.ui)
+    api(libs.androidx.ui.graphics)
+    api(libs.androidx.ui.tooling.preview)
+    api(libs.androidx.material3)
+    api(libs.androidx.material)
     androidTestApi(composeBom)
-    androidTestApi("androidx.compose.ui:ui-test-junit4")
-    debugApi("androidx.compose.ui:ui-tooling")
-    debugApi("androidx.compose.ui:ui-test-manifest")
+    androidTestApi(libs.androidx.ui.test.junit4)
+    debugApi(libs.androidx.ui.tooling)
+    debugApi(libs.androidx.ui.test.manifest)
 
-    api("androidx.compose.material3:material3-window-size-class")
+    api(libs.androidx.compose.material3.window.size.class4)
     // Integration with observables
-    api("androidx.compose.runtime:runtime-livedata")
+    api(libs.androidx.runtime.livedata)
     // Compose Material 图标
-    api("androidx.compose.material:material-icons-core")
-    api("androidx.compose.material:material-icons-extended")
+    api(libs.androidx.material.icons.core)
+    api(libs.androidx.material.icons.extended)
     // Integration with ViewModels
-    api("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
+    api(libs.androidx.lifecycle.viewmodel.compose)
     // 用于Compose的生命周期运行时
-    api("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
+    api(libs.androidx.lifecycle.runtime.compose)
     // navigation
-    api("androidx.navigation:navigation-compose:2.7.5")
+    api(libs.androidx.navigation.compose)
 
     // Accompanist: https://google.github.io/accompanist/
-    val accompanistVersion = "0.34.0"
     // 动态权限申请
-    api("com.google.accompanist:accompanist-permissions:$accompanistVersion")
+    api(libs.accompanist.permissions)
 //    // 权限请求框架 适配Android 14 https://github.com/getActivity/XXPermissions
 //    api 'com.github.getActivity:XXPermissions:18.5'
     // PermissionX 权限请求库
 //    api("com.guolindev.permissionx:permissionx:1.7.1")
 
     // 启动画面
-    api("androidx.core:core-splashscreen:1.0.0")
+    api(libs.androidx.core.splashscreen)
 
 
     implementation(files("libs/sun.misc.BASE64Decoder.jar"))
     // 约束布局
-    api("androidx.constraintlayout:constraintlayout:2.1.4")
+    api(libs.androidx.constraintlayout)
     // 添加Jetpack中架构组件的依赖，注意viewmodel要添加viewmodel-ktx的依赖
-    api("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    api("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    api(libs.androidx.lifecycle.livedata.ktx)
+    api(libs.androidx.lifecycle.viewmodel.ktx)
     // Kotlin Coroutines 协程
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    api(libs.coroutines)
     // ViewPager2
-    api("androidx.viewpager2:viewpager2:1.0.0")
+    api(libs.androidx.viewpager2)
     // 强大而灵活的RecyclerView Adapter
-    api("com.github.CymChad:BaseRecyclerViewAdapterHelper:3.0.6")
+    api(libs.baserecyclerviewadapterhelper)
     // Android 版本更新 https://github.com/AlexLiuSheng/CheckVersionLib
     // 原版已不再维护，未适配Android 12 会报java.lang.IllegalArgumentException错误
 //    api("com.github.AlexLiuSheng:CheckVersionLib:2.4.1_androidx")
-    api("com.github.jikun2008:CheckVersionLib:2.4.5")
+    // Android 版本更新 https://github.com/jikun2008/CheckVersionLib/
+    api(libs.checkversionlib)
 }
 
 afterEvaluate {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-/*buildscript {
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
-}*/
-
 plugins {
     // Android Gradle plugin and Android Studio 对应关系: https://developer.android.google.cn/studio/releases/
-    id("com.android.application") version "8.4.0" apply false
-    id("com.android.library") version "8.4.0" apply false
+//    id("com.android.application") version "8.4.0" apply false
+//    id("com.android.library") version "8.4.0" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
     // kotlin编译器与kotlin版本对应关系：https://developer.android.google.cn/jetpack/androidx/releases/compose-kotlin#kts
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false// kotlin插件版本
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+//    id("org.jetbrains.kotlin.android") version "1.9.22" apply false// kotlin插件版本
     // 声明 KSP 插件。请务必选择与项目的 Kotlin 版本一致的 KSP 版本。https://developer.android.google.cn/studio/build/migrate-to-ksp?hl=zh-cn
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+//    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    alias(libs.plugins.ksp) apply false
 }
