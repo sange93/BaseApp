26af07c93b6191d0c1cd727ed7f8b10ffa65952d
sangshengqi <sangsq@foxmail.com>
2024-09-24 17:04:34 +0800

    1、升级AGP 8.6.0; 2、修改ViewModel.launch返回job; 3、版本"1.1.12";

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.github.sange93"
-version = "1.1.11"
+version = "1.1.12"
 
 android {
     namespace = "com.sange.base"
@@ -127,7 +127,7 @@ afterEvaluate {
             create<MavenPublication>("release"){
                 groupId = "com.github.sange93"
                 artifactId = "BaseApp"
-                version = "1.1.11"
+                version = "1.1.12"
                 from(components["release"])
             }
         }
