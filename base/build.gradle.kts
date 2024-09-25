9b82487705bb9e1d3f85088b902290c0b2547c93
sangshengqi <sangsq@foxmail.com>
2024-09-25 10:57:50 +0800

    1、日志增加头部（文件行号）信息; 2、版本"1.1.3_compat_5";

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.github.sange93"
-version = "1.1.3"
+version = "1.1.3_compat_5"
 
 android {
     namespace = "com.sange.base"
@@ -129,7 +129,7 @@ afterEvaluate {
             create<MavenPublication>("release"){
                 groupId = "com.github.sange93"
                 artifactId = "BaseApp"
-                version = "1.1.3"
+                version = "1.1.3_compat_5"
                 from(components["release"])
             }
         }
--- base/src/main/java/com/sange/base/util/LogUtils.kt
+++ base/src/main/java/com/sange/base/util/LogUtils.kt
@@ -7,7 +7,7 @@ import com.sange.base.BaseApplication
  * 日志工具类
  *
  * 具体规则:
- * -Verbose等级的Log，请不要在user版本中出现。
+ * -Verbose debug 等级的Log，请不要在user版本中出现。
  * -Info、Warn、Error等级的Log禁止作为普通的调试信息使用，这些等级的Log是系统出现问题时候的重要分析线索，如果随意使用，将给Log分析人员带来极大困扰。
  * -Log的tag命名，使用Activity名称或者类、模块的名称，不要出现自己的姓名拼音或其他简称。
  * -Log输出的频率需要控制,例如1s打印一次的Log，尽量只在eng版本使用。
@@ -15,14 +15,14 @@ import com.sange.base.BaseApplication
  */
 object LogUtils{
     private const val TAG = "base_log"
+    /** 堆栈集合，下标偏移量 */
+    var mStackOffset = 0
 
     /**
      * Verbose: 开发调试过程中一些详细信息，不应该编译进产品中，只在开发阶段使用。
      */
     fun v(msg: String) {
-        if (BaseApplication.isDebugMode) {
-            Log.v(TAG, msg)
-        }
+        v(TAG, msg)
     }
 
     /**
@@ -30,7 +30,23 @@ object LogUtils{
      */
     fun v(tag: String, msg: String) {
         if (BaseApplication.isDebugMode) {
-            Log.v(tag, msg)
+            Log.v(tag, processHead(msg))
+        }
+    }
+
+    /**
+     * debug:开发调试。
+     */
+    fun d(msg: String) {
+        d(TAG, msg)
+    }
+
+    /**
+     * debug:开发调试。
+     */
+    fun d(tag: String, msg: String) {
+        if (BaseApplication.isDebugMode) {
+            Log.d(tag, processHead(msg))
         }
     }
 
@@ -38,9 +54,14 @@ object LogUtils{
      * Info:例如一些运行时的状态信息，这些状态信息在出现问题的时候能提供帮助。
      */
     fun i(tag: String, msg: String) {
-        if (BaseApplication.isDebugMode) {
-            Log.i(tag, msg)
-        }
+        Log.i(tag, processHead(msg))
+    }
+
+    /**
+     * Warn: 警告信息
+     */
+    fun w(tag: String, msg: String) {
+        Log.w(tag, processHead(msg))
     }
 
     /**
@@ -54,6 +75,20 @@ object LogUtils{
      * Error: 错误信息
      */
     fun e(tag: String, msg: String) {
-        Log.e(tag, msg)
+        Log.e(tag, processHead(msg))
+    }
+
+    /**
+     * 包装日志头部信息
+     */
+    private fun processHead(msg: String): String{
+        val stackTrace = Throwable().stackTrace
+        val stackIndex = 3 + mStackOffset
+        val targetElement = stackTrace[stackIndex].toString()
+        val tName = Thread.currentThread().name
+        return "┌────────────────────────────────────────────────────────\n" +
+                "$tName, $targetElement\n" +
+                "├┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄\n" +
+                msg
     }
 }
