477a602fb8856a678151aeb88950fc0affba5219
sangshengqi <sangsq@foxmail.com>
2025-03-24 14:01:18 +0800

    版本 1.2.2；

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.github.sange93"
-version = "1.2.0"
+version = "1.2.2"
 
 android {
     namespace = "com.sange.base"
@@ -127,7 +127,7 @@ afterEvaluate {
             create<MavenPublication>("release"){
                 groupId = "com.github.sange93"
                 artifactId = "BaseApp"
-                version = "1.2.0"
+                version = "1.2.2"
                 from(components["release"])
             }
         }
