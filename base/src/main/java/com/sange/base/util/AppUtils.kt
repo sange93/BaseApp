52e58cf5a65a593a584bb9b3abfa940a3cbad9e6
sangshengqi <sangsq@foxmail.com>
2025-07-24 15:01:24 +0800

    1、升级agp = "8.11.1"； 2、升级gradle-8.14-all； 3、增加定时拉起应用工具方法；

--- base/src/main/java/com/sange/base/util/AppUtils.kt
+++ base/src/main/java/com/sange/base/util/AppUtils.kt
@@ -1,5 +1,7 @@
 package com.sange.base.util
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.pm.ApplicationInfo
@@ -24,15 +26,12 @@ object AppUtils {
      * @param context
      */
     fun restartApp(context: Context) {
-        val packageManager = context.packageManager
-        if (null == packageManager) {
-            LogUtils.e("null == packageManager")
-            return
-        }
-        val intent = packageManager.getLaunchIntentForPackage(context.packageName)
+        val intent = context.packageManager.getLaunchIntentForPackage(context.packageName)
         if (intent != null) {
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             context.startActivity(intent)
+        }else{
+            LogUtils.e("AppUtils restartApp() error")
         }
     }
 
@@ -49,9 +48,11 @@ object AppUtils {
 
     /**
      * 延时重启APP
+     * @param time 秒 触发时间 默认5秒
+     * @param clazz 启动的Activity 示例：com.xxx.xxx.MainActivity
      */
-    fun restartApp() {
-        val cmd = "sleep 5; am start -N com.zjzn.vending.MainActivity"
+    fun restartApp(time: Int = 5, clazz: String) {
+        val cmd = "sleep ${time}; am start -N $clazz"
         val commandResult = ShellUtils.execCommand(cmd, true, true)
         LogUtils.e(
             StringBuilder().append("restartApp() successMsg:").append(commandResult.successMsg)
@@ -60,8 +61,32 @@ object AppUtils {
     }
 
     /**
+     * 定时拉起应用(3秒)
+     * @param time 秒 触发时间 默认3秒
+     * @param clazz 启动的Activity 示例：MainActivity::class.java
+     */
+    fun autoRunApp(time: Int = 3, clazz: Class<Any>) {
+        val context = Base.getContext()
+        // 获取AlarmManager服务
+        val alarmManager =
+            context.getSystemService(Context.ALARM_SERVICE) as AlarmManager?
+        // 设置触发时间 3秒
+        val triggerAtMillis = System.currentTimeMillis() + time * 1000//1 * 60 * 1000
+        // 创建Intent，用于指定要启动的Activity或Service
+        val intent = Intent(context, clazz)
+        // 使用PendingIntent包装Intent
+        val pendingIntent = PendingIntent.getActivity(
+            context, 0, intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        // 设置单次闹钟
+        alarmManager?.set(AlarmManager.RTC_WAKEUP, triggerAtMillis, pendingIntent)
+    }
+
+    /**
      * 是否为APP进程
      * @param appPackageName APP包名
      */
-    fun isAppProcess(appPackageName: String) = appPackageName == ProcessUtils.getProcessName(Base.getContext(), android.os.Process.myPid())
+    fun isAppProcess(appPackageName: String) =
+        appPackageName == ProcessUtils.getProcessName(Base.getContext(), android.os.Process.myPid())
 }
