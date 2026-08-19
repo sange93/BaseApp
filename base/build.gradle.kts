dc998e2ccac9348254ef16cb056276c41d4c411a
sangshengqi <sangshengqi@qltyx.com>
2026-08-19 15:51:26 +0800

    chore(base): 更新版本号到 1.2.12
    - 将版本号从 1.2.11 更新为 1.2.12

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -131,7 +131,7 @@ publishing {
         create<MavenPublication>("release"){
             groupId = "com.github.sange93"
             artifactId = "BaseApp"
-            version = "1.2.11"
+            version = "1.2.12"
 //                from(project.components["release"])
             afterEvaluate {
                 from(components["release"])
